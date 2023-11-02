bb9d8f852d7b9c00d4b61f471a612729f11b1d9e
StrunkGroove <jmarkj@mail.ru>
2023-11-02 15:38:24 +0300

    Small change

--- web/djangodocker/settings.py
+++ web/djangodocker/settings.py
@@ -1,11 +1,7 @@
 import os
 
-from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
-
-load_dotenv(dotenv_path)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
